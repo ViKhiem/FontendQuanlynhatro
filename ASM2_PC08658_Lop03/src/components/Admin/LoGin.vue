6d5b58a4ab24255f7d80f87a2f9a3081d9272180
ViKhiem <163488262+ViKhiem@users.noreply.github.com>
2024-12-10 22:38:55 +0700

    update login

--- ASM2_PC08658_Lop03/src/components/Admin/LoGin.vue
+++ ASM2_PC08658_Lop03/src/components/Admin/LoGin.vue
@@ -1,14 +1,50 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+import Swal from 'sweetalert2'
+import { useRoute } from 'vue-router'
+
+const API_JSON = 'http://localhost:3000/users'
+const router = useRoute()
+const account = ref({})
+
+function login() {
+  console.log(account.value.username)
+  console.log(account.value.password)
+  axios
+    .get(API_JSON + '?username=' + account.value.username)
+    .then(function (response) {
+      const data = response.data
+      const accountAPI = data
+      console.log(accountAPI)
+      if (account.value.password == accountAPI.password) {
+        console.log('Đúng mật khẩu')
+
+        Swal.fire({
+          title: 'Thành công!',
+          text: 'Đăng nhập thành công',
+          icon: 'success',
+          confirmButtonText: 'Ok',
+        }).then((result) => {
+          if (result.isConfirmed) {
+            router.push('/')
+          }
+        })
+      }
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+}
+</script>
 
 <template>
   <main>
     <div class="container">
-      <!-- Section: Login Form -->
       <section class="text-center text-lg-start mt-5">
         <div class="card mb-3">
           <h1 class="text-center text-primary mt-2">ĐĂNG NHẬP</h1>
           <div class="row g-0 d-flex align-items-center">
-            <!-- Image Column -->
             <div class="col-lg-4 d-none d-lg-flex">
               <img
                 src="../../assets/image/LogoNVK.jpg"
@@ -16,19 +52,18 @@
                 class="w-100 rounded-t-5 rounded-tr-lg-0 rounded-bl-lg-5"
               />
             </div>
-            <!-- Form Column -->
             <div class="col-lg-8">
               <div class="card-body py-5 px-md-5">
-                <form>
-                  <!-- Email Input -->
+                <form v-on:submit.prevent="login">
                   <div class="mb-3">
-                    <label for="phone" class="form-label">Số điện thoại</label>
+                    <label for="phone" class="form-label">Username</label>
                     <input
+                      v-model="account.username"
                       type="tel"
                       class="form-control"
                       name="phone"
                       id="phone"
-                      placeholder="Số điện thoại"
+                      placeholder="Username"
                     />
                     <small id="helpId" class="form-text text-muted"></small>
                   </div>
@@ -37,6 +72,7 @@
                   <div class="mb-3">
                     <label for="password" class="form-label">Nhập mật khẩu</label>
                     <input
+                      v-model="account.password"
                       type="password"
                       class="form-control"
                       name="password"
@@ -45,15 +81,7 @@
                     />
                     <small id="helpId" class="form-text text-muted"></small>
                   </div>
-                  <a
-                    name="login"
-                    id="login"
-                    class="btn btn-outline-info mb-3"
-                    href="login.html"
-                    role="button"
-                  >
-                    <RouterLink to="/login" class="dropdown-item">Đăng nhập</RouterLink>
-                  </a>
+                  <button type="submit" class="btn btn-outline-info mb-3">Đăng nhập</button>
                 </form>
               </div>
             </div>
